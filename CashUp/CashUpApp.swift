41bf0bd74c260b9d8480047313d97a648b36f99f
gustavosouto02 <gugasouto2004@gmail.com>
2025-05-08 20:47:11 -0300

    Initial Commit

---
+++ CashUp/CashUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashUpApp.swift
+//  CashUp
+//
+//  Created by Gustavo Souto Pereira on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CashUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CashUp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CashUp
+//
+//  Created by Gustavo Souto Pereira on 08/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
